118e6183ee16813808e3205e690785e2967fc816
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-21 18:41:12 +0530

    api config setup

---
+++ src/http/api.ts
@@ -0,0 +1,8 @@
+import { Credentials } from "../types";
+import { api } from "./client";
+
+// Auth service
+export const login = async (credentials: Credentials) =>
+  await api.post(`/auth/login`, credentials);
+
+export const self = async () => await api.get(`/auth/self`);
