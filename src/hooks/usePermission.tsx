7d5f17a8f0ee5ca22dcab1c4327f1bcaa2c50fe6
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-21 20:21:19 +0530

    add custom usePermission hook

---
+++ src/hooks/usePermission.tsx
@@ -0,0 +1,17 @@
+import { User } from "../store";
+
+export const usePermission = () => {
+  const allowedRoles = ["admin", "manager"];
+
+  const _hasPermission = (user: User | null): boolean => {
+    if (user) {
+      return allowedRoles.includes(user.role);
+    }
+
+    return false;
+  };
+
+  return {
+    isAllowed: _hasPermission,
+  };
+};
