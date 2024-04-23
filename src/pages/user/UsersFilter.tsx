307f0dc0f5d0acce4cbf384d8ca0f8847a5d3c0e
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-23 19:43:13 +0530

    add filter select component

--- src/pages/user/UsersFilter.tsx
+++ src/pages/user/UsersFilter.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Input, Row } from "antd";
+import { Card, Col, Form, Input, Row, Select } from "antd";
 import React from "react";
 
 type UserFilterProps = {
@@ -18,6 +18,32 @@ const UsersFilter = ({ onFilterChange, children }: UserFilterProps) => {
                 allowClear
                 onChange={(e) => onFilterChange("searchFilter", e.target.value)}
               />
+            </Col>
+            <Col span={8}>
+              <Form.Item name="role">
+                <Select
+                  style={{ width: "100%" }}
+                  allowClear={true}
+                  placeholder="Select role"
+                >
+                  <Select.Option value="admin">Admin</Select.Option>
+                  <Select.Option value="manager">Manager</Select.Option>
+                  <Select.Option value="customer">Customer</Select.Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Select
+                style={{ width: "100%" }}
+                placeholder="Status"
+                allowClear={true}
+                onChange={(selectedItem) =>
+                  onFilterChange("statusFilter", selectedItem)
+                }
+              >
+                <Select.Option value="ban">Ban</Select.Option>
+                <Select.Option value="active">Active</Select.Option>
+              </Select>
             </Col>
           </Row>
         </Col>
