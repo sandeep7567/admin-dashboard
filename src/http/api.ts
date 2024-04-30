9250aa8e8ef66fe3a0cf201496f2505f8307c2ef
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-30 19:56:33 +0530

    add api gateway

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,36 +1,41 @@
 import { CreateTenantData, CreateUserData, Credentials } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+export const CATALOG_SERVICE = "/api/catalog";
+
 // Auth service
 export const login = async (credentials: Credentials) =>
-  await api.post(`/auth/login`, credentials);
+  await api.post(`${AUTH_SERVICE}/auth/login`, credentials);
 
-export const self = async () => await api.get(`/auth/self`);
+export const self = async () => await api.get(`${AUTH_SERVICE}/auth/self`);
 
-export const logout = async () => await api.post(`/auth/logout`);
+export const logout = async () => await api.post(`${AUTH_SERVICE}/auth/logout`);
 
 export const getUsers = async (queryString: string) =>
-  await api.get(`/users?${queryString}`);
+  await api.get(`${AUTH_SERVICE}/users?${queryString}`);
 
 export const getTenants = async (queryString: string) =>
-  await api.get(`/tenants?${queryString}`);
+  await api.get(`${AUTH_SERVICE}/tenants?${queryString}`);
 
 export const createUser = async (user: CreateUserData) =>
-  await api.post(`/users`, user);
+  await api.post(`${AUTH_SERVICE}/users`, user);
 
 export const createTenant = async (tenant: CreateTenantData) =>
-  await api.post(`/tenants`, tenant);
+  await api.post(`${AUTH_SERVICE}/tenants`, tenant);
 
 export const updateUser = async (user: CreateUserData, id: string) =>
-  await api.patch(`/users/${id}`, user);
+  await api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 
 export const deleteUser = async (id: string) =>
-  await api.delete(`/users/${id}`);
+  await api.delete(`${AUTH_SERVICE}/users/${id}`);
 
 export const updateTenant = async (
   tenant: CreateTenantData,
   tenantId: string
-) => await api.patch(`/tenants/${tenantId}`, tenant);
+) => await api.patch(`${AUTH_SERVICE}/tenants/${tenantId}`, tenant);
 
 export const deleteTenant = async (tenantId: string) =>
-  await api.delete(`/tenants/${tenantId}`);
+  await api.delete(`${AUTH_SERVICE}/tenants/${tenantId}`);
+
+// Catalog service
--- src/http/client.ts
+++ src/http/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useAuthStore } from "../store";
+import { AUTH_SERVICE } from "./api";
 
 // api instance axios 1
 export const api = axios.create({
@@ -14,7 +15,7 @@ export const api = axios.create({
 // api instance axios 2
 const refreshToken = async () => {
   await axios.post(
-    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    `${import.meta.env.VITE_BACKEND_API_URL}${AUTH_SERVICE}/auth/refresh`,
     {},
     {
       withCredentials: true,
