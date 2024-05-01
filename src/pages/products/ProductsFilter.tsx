82ae3293f833df8c2d5414e7bbd4ec0aa7a2f999
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-01 14:39:19 +0530

    fix switch aligment label

--- src/pages/products/ProductsFilter.tsx
+++ src/pages/products/ProductsFilter.tsx
@@ -84,7 +84,9 @@ const ProductsFilter = ({ children }: UserFilterProps) => {
                 <Form.Item name={"isPublish"}>
                   <Switch defaultChecked onChange={() => {}} />
                 </Form.Item>
-                <Typography.Text>Show only published</Typography.Text>
+                <Typography.Text style={{ marginBottom: 25, display: "block" }}>
+                  Show only published
+                </Typography.Text>
               </Space>
             </Col>
           </Row>
